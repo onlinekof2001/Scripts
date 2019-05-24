89c54539ca0fe5df760fd4ffef68f118333895fe
onlinekof2001 <onlinekof2001@aliyun.com>
2019-05-24 14:14:47 +0800

    benchmark test

--- Benchmark/pgbench_DB.sh
+++ Benchmark/pgbench_DB.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 pgbch='/usr/pgsql-10/bin/pgbench'
+psqlc='/usr/pgsql-10/bin/psql'
+versn=$(hostnamectl | awk '/Operating System/{ print $(NF-1)}')
+osbrd=$(hostnamectl | grep -c 'Red Hat')
 
 function timestamp {
     date +'%b %d %H:%M:%S'
@@ -9,17 +12,43 @@ function timestamp {
 function log {
     TYPE=$(echo $1 | tr '[:lower:]' '[:upper:]')
     shift
-    echo "$(timestamp) $TYPE: ${*}" >> ${LOGFILE}
+    echo "$(timestamp) $TYPE: ${*}" | tee -a ${LOGFILE}
     if [[ "${TYPE}" == "FATAL" ]]; then
         exit 1
     fi
 }
 
-hst='172.31.19.42'
-prt='60901'
+
+if [[ ! -x $pgbch ]]
+then
+   if [[ $versn -le 7 ]] || [[ $osbrd -eq 0 ]]
+   then
+       log print "Install pgbench for PostgreSQL on CentOS"
+       yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
+       yum install -y postgresql10-contrib
+   else
+	   log print "Install pgbench for PostgreSQL on Red Hat"
+       yum install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-2.noarch.rpm
+       yum install -y postgresql10-contrib
+   fi
+fi
+
+hst='12.xxx.xxx.xxx'
+prt='xxxx'
 usr='performance'
 pwd='pgsqladmin'
 dbn='performance'
+
+function init_pgbench {
+	pghst=$1
+    pgprt=$2
+    pgusr=$3
+    pgdbn=$5
+	pgscl=$6
+    export PGPASSWORD=$4
+	$psqlc -h${pghst} -p${pgprt} -U${pgusr} postgres -c "create database performance"
+    $pgbch -i -s ${pgscl} -h${pghst} -p${pgprt} -U${pgusr} ${pgdbn} 
+}
 
 function run_pgbench {
     pghst=$1
@@ -27,22 +56,30 @@ function run_pgbench {
     pgusr=$3
     pgdbn=$5
     pgtrd=$6
+	pgtim=$7
     export PGPASSWORD=$4
-    $pgbch -M prepared -n -h${pghst} -p${pgprt} -U${pgusr} ${pgdbn} -T5 -c${pgtrd} -j${pgtrd}
+    $pgbch -M prepared -b -n -h${pghst} -p${pgprt} -U${pgusr} ${pgdbn} -T${pgtim} -c${pgtrd} -j${pgtrd}
 }
 
 
 LOGFILE=$(mktemp /tmp/pgbench_log.XXXXXXXXX)
 
-thrd=40
-while ((${thrd} < 480))
-do
-  run_pgbench ${hst} ${prt} ${usr} ${pwd} ${dbn} ${thrd} | tee -a ${LOGFILE}
-  if [[ ${thrd} -lt 400 ]]
-  then
-      let thrd*=2
-  else
-      let thrd+=80
-  fi
+read -p "Initial test database [y|n]: " opt
+opt=$(echo ${opt} | tr '[:upper:]' '[:lower:]')
 
+if [[ $opt = 'y' ]]
+then 
+read -p "scale * 100000 = table_rows [Value of Scale]: " scale
+init_pgbench ${hst} ${prt} ${usr} ${pwd} ${dbn} ${scale}
+fi
+
+read -p "Max concurrency threads [ 240 - 1260 ]: " conns
+thrd=$conns
+
+time=600
+for ((i=3;i>0;i++))
+do
+  last_time=$((${i}*${time}))
+  run_pgbench ${hst} ${prt} ${usr} ${pwd} ${dbn} ${thrd} ${last_time} | tee -a ${LOGFILE}
+  sleep $((${time}/10))s
 done
--- Benchmark/sysbench_DB.sh
+++ Benchmark/sysbench_DB.sh
@@ -15,10 +15,41 @@ function log {
     fi
 }
 
+read -p "Choose SQL drive [ mysql | pgsql ]" sqldrv
+
+if [ $sqldrv = 'mysql' ]
+then
 hst='mysql5725.cdncaqbxqkyv.rds.cn-north-1.amazonaws.com.cn'
 prt='3306'
 usr='mysqladmin'
 dbn='performance'
+pwd=$usr
+else
+hst='postgresqlbench.postgres.database.chinacloudapi.cn'
+prt='5432'
+usr='pgsqladmin@'
+dbn='performance'
+pwd='Decathlon2018'
+fi
+
+function init_sysbench {
+    mysqlhst=$1
+    mysqlprt=$2
+    mysqlusr=$3
+    mysqlpwd=$4
+    mysqldbn=$5
+    mysqlrwo=$6
+    mysqlrow=$7
+    if [ $sqldrv = 'mysql' ]
+    then
+        $mysqlc -h${mysqlhst} -P${mysqlprt} -u${mysqlusr} -D mysql --password=${mysqlpwd} -e "create database ${mysqldbn}"
+        $sysb --mysql-host=${mysqlhst} --mysql-port=${mysqlprt} --mysql-user=${mysqlusr} --mysql-password=${mysqlpwd} --mysql-db=${mysqldbn} ${mysqlrwo} --table_size ${mysqlrow}
+    else
+        export PGPASSWORD=${mysqlpwd}
+        $psqlc -h${mysqlhst} -p${mysqlprt} -U${mysqlusr} postgres -c "create database ${mysqldbn}"
+        $sysb --db-driver=pgsql --pgsql-host=${mysqlhst} --pgsql-port=${mysqlprt} --pgsql-user=${mysqlusr} --pgsql-password=${mysqlpwd} --pgsql-db=${mysqldbn} ${mysqlrwo} --table_size ${mysqlrow}
+    fi
+}
 
 function run_sysbench {
     mysqlhst=$1
@@ -28,15 +59,34 @@ function run_sysbench {
     mysqldbn=$5
     mysqltrd=$6
     mysqlrwo=$7
-    $sysb --mysql-host=${mysqlhst} --mysql-port=${mysqlprt} --mysql-user=${mysqlusr} --mysql-password=${mysqlpwd} --mysql-db=${mysqldbn} --threads=${mysqltrd} --time=180 --thread-stack-size=128k --percentile=0 ${mysqlrwo} run
+	mysqltim=$8
+    if [ $sqldrv = 'mysql' ]
+    then
+    $sysb --mysql-host=${mysqlhst} --mysql-port=${mysqlprt} --mysql-user=${mysqlusr} --mysql-password=${mysqlpwd} --mysql-db=${mysqldbn} --threads=${mysqltrd} --time=${mysqltim} --thread-stack-size=128k --percentile=0 ${mysqlrwo} run
+    else
+    $sysb --db-driver=pgsql --pgsql-host=${mysqlhst} --pgsql-port=${mysqlprt} --pgsql-user=${mysqlusr} --pgsql-password=${mysqlpwd} --pgsql-db=${mysqldbn} --threads=${mysqltrd} --time=${mysqltim} --percentile=0 ${mysqlrwo} run
+    fi
 }
 
-LOGFILE=$(mktemp /tmp/sysbench_log.XXXXXXXXX)
+LOGFILE=$(mktemp /tmp/sysbench_log_${sqldrv}.XXXXXXXXX)
+read -p "Initial test database [y|n]: " opt
+opt=$(echo ${opt} | tr '[:upper:]' '[:lower:]')
+
 read -p "Choose type [ oltp_read_only | oltp_read_write ]" rw_type
 
-thrd=80
-while ((${thrd} <= 480))
+if [[ $opt = 'y' ]]
+then 
+read -p "table_rows [Value of rows]: " scale
+init_sysbench ${hst} ${prt} ${usr} ${pwd} ${dbn} ${scale}
+fi
+
+read -p "Max concurrency threads [ 240 - 1260 ]: " conns
+thrd=$conns
+
+time=600
+for ((i=3;i>0;i++))
 do
-  run_sysbench ${hst} ${prt} ${usr} ${usr} ${dbn} ${thrd} ${rw_type} | tee -a ${LOGFILE}
-  let thrd+=80
+  last_time=$((${i}*${time}))
+  run_sysbench ${hst} ${prt} ${usr} ${pwd} ${dbn} ${thrd} ${rw_type} ${last_time} | tee -a ${LOGFILE}
+  sleep $((${time}/10))s
 done
